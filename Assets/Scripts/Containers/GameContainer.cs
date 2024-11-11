5a9050987b45cafd0e89c49458ac731714d902d6
Kasia Jaromirska <kasia.jaromirska@gmail.com>
2024-11-11 01:59:45 +0100

    Game over screen

--- Assets/Scripts/Containers/GameContainer.cs
+++ Assets/Scripts/Containers/GameContainer.cs
@@ -1,6 +1,7 @@
 using TMPro;
 using UnityEngine;
 using Mirror;
+using UnityEngine.UI;
 
 public class GameContainer : NetworkBehaviour
 {
@@ -18,16 +19,45 @@ public class GameContainer : NetworkBehaviour
     [SyncVar(hook = nameof(OnPlayer2ScoreChanged))]
     private int player2Score = 0;
 
-    public void UpdateScore(int playerID, int score)
+    public override void OnStartClient()
     {
-        Debug.Log("Updating score");
-        if (playerID == 0)
+        GameTimer.OnTimerEnd.AddListener(HandleGameOver);
+    }
+
+    private void HandleGameOver()
+    {
+        Debug.Log("Time is up!");
+        gameOverScreen.SetActive(true);
+        TextMeshProUGUI resultText = gameOverScreen.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        Debug.Log(player1Score + " i " + player2Score);
+        if (player1Score > player2Score)
         {
-            player1Score += 10;
+            resultText.text = "Player 1 wins!!";
         }
         else
         {
-            player2Score += 10;
+            resultText.text = "Player 2 wins!!";
+        }
+
+        if (isServer)
+        {
+            Button mainMenuButton = gameOverScreen.transform.GetChild(2).GetComponent<Button>();
+            mainMenuButton.onClick.AddListener(() =>
+            {
+                Debug.Log("here we go to main menu - add reference to CustomNetworkManager")
+            });
+        }
+    }
+
+    public void UpdateScore(int playerID, int score)
+    {
+        if (playerID == 0)
+        {
+            player1Score += score;
+        }
+        else
+        {
+            player2Score += score;
         }
     }
 
